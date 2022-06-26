b8953124f80f54e68445b01c87dfce0ed16df672
Akhlaque <akhlaquea01@gmail.com>
2022-06-27 00:40:13 +0530

    Distributed In Multiple Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { ServicesSectionComponent } from './components/services-section/services-section.component';
+import { MySkillsComponent } from './components/my-skills/my-skills.component';
+import { TestimonialsSectionComponent } from './components/testimonials-section/testimonials-section.component';
+import { MyWorkSectionComponent } from './components/my-work-section/my-work-section.component';
+import { ContactSectionComponent } from './components/contact-section/contact-section.component';
+import { SocialSectionComponent } from './components/social-section/social-section.component';
+import { HomeSectionComponent } from './components/home-section/home-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutSectionComponent,
+    ServicesSectionComponent,
+    MySkillsComponent,
+    TestimonialsSectionComponent,
+    MyWorkSectionComponent,
+    ContactSectionComponent,
+    SocialSectionComponent,
+    HomeSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/social-section/social-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SocialSectionComponent } from './social-section.component';
+
+describe('SocialSectionComponent', () => {
+  let component: SocialSectionComponent;
+  let fixture: ComponentFixture<SocialSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SocialSectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SocialSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
