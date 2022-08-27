8eefa31c9faf40458139f59b3b12286ce5bbc190
Akhlaque <akhlaquea01@gmail.com>
2022-08-27 16:21:41 +0530

    Responsive Updated

--- src/app/components/testimonials-section/testimonials-section.component.ts
+++ src/app/components/testimonials-section/testimonials-section.component.ts
@@ -1,79 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import {  Autoplay, SwiperOptions } from 'swiper';
+import { Autoplay, SwiperOptions } from 'swiper';
+import { HostListener } from "@angular/core";
+import { fromEvent, Observable, Subscription } from "rxjs";
 
 
-import SwiperCore, { EffectCoverflow, Pagination,  } from "swiper";
+import SwiperCore, { EffectCoverflow, Pagination, } from "swiper";
 
 
 // install Swiper modules
 
 
 import Swiper from 'swiper';
+import { event } from 'jquery';
 
-SwiperCore.use([  
+SwiperCore.use([
   Pagination,
   Autoplay,
   EffectCoverflow,
 
-  
-
-
-  
-  
-  
 ]);
+
 
 @Component({
   selector: 'app-testimonials-section',
   templateUrl: './testimonials-section.component.html',
-  styleUrls: ['./testimonials-section.component.scss']
+  styleUrls: ['./testimonials-section.component.scss'],  
 })
 export class TestimonialsSectionComponent implements OnInit {
- 
-  
-  
+  screenHeight: number = 0;
+  screenWidth: number = 0;
+  count = 3  
+  resizeObservable$: Observable<Event> | undefined
+  resizeSubscription$: Subscription | undefined
 
-  constructor() { }
-
-  ngOnInit(): void {
-
-
+  constructor() {
     
-  
-
-    
-
- 
-
-    // var swiper = new Swiper(".slide-content", {
-    //   slidesPerView: 2,
-    //   spaceBetween: 25,
-    //   loop: true,
-    //   centeredSlides: true,
-    //   // fadeEffect: true,
-    //   grabCursor: true,
-    //   pagination: {
-    //     el: ".swiper-pagination",
-    //     clickable: true,
-    //     dynamicBullets: true,
-    //   },
-    //   navigation: {
-    //     nextEl: ".swiper-button-next",
-    //     prevEl: ".swiper-button-prev",
-    //   },
-  
-    //   breakpoints:{
-    //       0: {
-    //           slidesPerView: 1,
-    //       },
-    //       520: {
-    //           slidesPerView: 2,
-    //       },
-    //       950: {
-    //           slidesPerView: 3,
-    //       },
-    //   },
-    // });
   }
-
+  ngOnInit(): void {
+    this.responsiveScreens();
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe(evt => {
+      this.responsiveScreens();      
+      console.log('event: ', evt)
+    })
+  }
+  responsiveScreens(){
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 360) {
+      this.count = 1
+    }
+    else if (this.screenWidth <= 540) {
+      this.count = 2
+    }
+    else{
+      this.count = 3;
+    }
+  }
 }
