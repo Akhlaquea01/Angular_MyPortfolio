595d2fb04f6633f959db8a2592251bdccf3bb032
Akhlaque <akhlaquea01@gmail.com>
2022-08-28 14:15:04 +0530

    Updated Skills

--- src/app/components/my-skills/my-skills.component.ts
+++ src/app/components/my-skills/my-skills.component.ts
@@ -7,15 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent implements OnInit {
-  
-  
-  
+  skills=[
+    "/assets/images/Jquery.png",
+    "/assets/images/Css.png",
+    "/assets/images/Angular.png",
+    "/assets/images/Git.png",
+    "/assets/images/Html.png",
+    "/assets/images/JS.png",
+    "/assets/images/TS.png",
+    "/assets/images/visual-studio-code-icon.png",
+    "/assets/images/React.png",
+    "/assets/images/Scss.png",//
+    "/assets/images/NodeJs.png",//
+    "/assets/images/MongoDb.png",//
+  ]
+
+
 
   constructor() { }
 
   ngOnInit(): void {
-    
-    
+
+
   }
 
 }
